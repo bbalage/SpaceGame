04afa4d49a03e87a9ab6af2e1c338d66ce5477a2
91leinaD <d.daniel910617@gmail.com>
2021-09-20 18:06:27 +0200

    The spaceship texture rate and the game resolution was changed

--- js/logToCanvas.js
+++ js/logToCanvas.js
@@ -1,5 +1,5 @@
 function drawRotation(spaceShip) {
-    ctx.font = "8px Arial";
+    ctx.font = "24px Arial";
     ctx.fillStyle = "#0095DD";
-    ctx.fillText("Rotation: " + spaceShip.rotation, canvas.width-65, 20);
+    ctx.fillText("Rotation: " + spaceShip.rotation, canvas.width/2-40, canvas.height / 2);
 }
