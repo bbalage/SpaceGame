9f1d075e4251a8293f245e0cffcf7e7d58927f7c
MrTitusZ <dariusz4434@gmail.com>
2021-09-21 19:18:28 +0200

    Removed decimal point

--- js/logToCanvas.js
+++ js/logToCanvas.js
@@ -2,6 +2,6 @@ function drawRotation(spaceShip) {
     ctx.font = "24px Arial";
     ctx.fillStyle = "#0095DD";
     ctx.fillText("Rotation: " + spaceShip.rotation, canvas.width * 0.8, canvas.height * 0.1);
-    ctx.fillText("X: " + spaceship.x.toFixed(2), canvas.width * 0.8, canvas.height * 0.15);
-    ctx.fillText("Y: " + spaceship.y.toFixed(2), canvas.width * 0.8, canvas.height * 0.2);
+    ctx.fillText("X: " + spaceship.x.toFixed(0), canvas.width * 0.8, canvas.height * 0.15);
+    ctx.fillText("Y: " + spaceship.y.toFixed(0), canvas.width * 0.8, canvas.height * 0.2);
 }
