959c4001d3b39955996654d079be0c51c6b17379
MrTitusZ <dariusz4434@gmail.com>
2021-09-21 19:17:04 +0200

    Cleared Unused Variables

--- js/global.js
+++ js/global.js
@@ -1,13 +1,8 @@
 const canvas = document.getElementById("c");
 const ctx = canvas.getContext("2d");
 
-nativeWidth = 1920;
-nativeHeight = 1080;
-
 deviceWidth = window.innerWidth;
 deviceHeight = window.innerHeight;
-
-
 
 canvas.width = deviceWidth;
 canvas.height = deviceHeight;
