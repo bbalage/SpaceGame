70f3aa65d9d4013c6fb72d57fa3e2aef78a99ec0
bbalage <bbalage0@gmail.com>
2021-12-12 19:01:05 +0100

    Added multiple spaceship rotation tests.

--- spec/spaceship.spec.js
+++ spec/spaceship.spec.js
@@ -1,10 +1,94 @@
-describe("Player", function() {
-    it("Check whether the spaceship turning function works fine.", function () {
-        const testCanvas = {"width": 10, "height": 10}
-        const testSpaceship = new Spaceship(testCanvas);
+describe("Spaceship unit tests", function() {
+
+    const startingX = 10;
+    const startingY = 10;
+
+    it("Spaceship turning function works fine clockwise; basic setting.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
         testSpaceship.rotation = 10;
-        Spaceship.elementaryTurn = 5;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 15;
+
         testSpaceship.rotate(1);
-        expect(testSpaceship.rotation).toBe(15);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine anti-clockwise; basic setting.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 10;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 5;
+
+        testSpaceship.rotate(-1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with zero direction.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 10;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 15;
+
+        testSpaceship.rotate(0);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with clockwise turn-around.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 358;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 3;
+
+        testSpaceship.rotate(1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with anti-clockwise turn-around.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 2;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 357;
+
+        testSpaceship.rotate(-1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with zero starting rotation anti-clockwise.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 0;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 355;
+
+        testSpaceship.rotate(-1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with zero starting rotation clockwise.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 0;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 5;
+
+        testSpaceship.rotate(1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
+    });
+
+    it("Spaceship turning function works fine with multiple calls.", function () {
+        const testSpaceship = new Spaceship(startingX, startingY);
+        testSpaceship.rotation = 0;
+        testSpaceship.elementaryTurn = 5;
+        const expectedRotation = 10;
+
+        testSpaceship.rotate(1);
+        testSpaceship.rotate(1);
+
+        expect(testSpaceship.rotation).toBe(expectedRotation);
     });
 })
